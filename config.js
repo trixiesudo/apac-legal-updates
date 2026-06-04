e7da7da776d5c444b0d7a0fd55981f2b7d401e61
trixiesudo <bealsw1114@gmail.com>
2026-06-05 04:51:09 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
 window.APAC_LEGAL_UPDATES_CONFIG = {
   apiBaseUrl: window.location.hostname.endsWith('github.io')
-  ? 'https://apac-legal-updates.onrender.com'
-  : 'http://127.0.0.1:8000',
-  publicMode: window.location.hostname.endsWith('github.io'),
+    ? 'https://apac-legal-updates.onrender.com'
+    : 'http://127.0.0.1:8005',
+  localApiBaseUrl: 'http://127.0.0.1:8005',
+  publishApiBaseUrl: 'https://apac-legal-updates.onrender.com',
+  publicMode: false,
 };
